72148b12755bbc965acdf9f146bd145ecbab8bef
smit vasoya <amitvasoya3110@gmail.com>
2020-11-21 22:40:46 +0530

    UI Part
    Added Views.py  at high level, templates, home page static files.

--- user_profile/signals.py
+++ user_profile/signals.py
@@ -15,7 +15,7 @@ def post_save_create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=Relationship)
 def post_save_add_to_friend(sender, instance, created, **kwargs):
     sender_ = instance.sender
-    receiver_ = insance.receiver
+    receiver_ = instance.receiver
     if instance.status == 'accepted':
         sender_.friends.add(receiver_.user)
         receiver_.friends.add(sender_.user)
