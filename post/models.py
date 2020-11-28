86ffe89b2818e0296ba4f9ab86353ded5cce9354
smit vasoya <amitvasoya3110@gmail.com>
2020-11-28 11:54:48 +0530

    Model_Method
    Methods Added To the Profile Model and Post Model

--- post/models.py
+++ post/models.py
@@ -4,19 +4,22 @@ from user_profile.models import Profile
 # Create your models here.
 class Post(models.Model):
     content =  models.TextField()
-    image = models.ImageField(upload_to='posts', validators=[FileExtensionValidator('png','jpg','jpeg')])
-    liked = models.ManyToManyField(Profile, default=None,related_name='likes')
-    updated = models.DateField(auto_created=True)
+    image = models.ImageField(upload_to='posts', validators=[FileExtensionValidator('png','jpg','jpeg')],blank=True)
+    liked = models.ManyToManyField(Profile, blank=True ,related_name='likes')
+    updated = models.DateField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
-        return str(self.content[:20])
+        return str(self.content[:40])
 
     def num_likes(self):
         return self.liked.all().count()
 
     #num of comments:
+    def num_comments(self):
+        return self.comment_set.all().count()
+
 
     class Meta:
         ordering = ('-created',)
