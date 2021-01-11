9054c07844942aeb038fb55349ff5665657cacc1
smit <ssssmmmmiiiitttt@gmail.com>
2021-01-11 16:19:27 +0530

    Home view login required

--- bffbook/views.py
+++ bffbook/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home_view(request):
     user = request.user
-    hello = 'hello world'
+    hello = 'hello'
 
     context ={'user':user,'hello':hello}
     return render(request, 'main/home.html', context)
